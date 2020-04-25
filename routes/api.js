89cee4870e7a2628dfe7b6e0f8ccf9465f9b2637
Glitch (spangle-sky-dandelion) <none>
2020-04-25 10:37:03 +0000

    🏚✨ Checkpoint ./routes/api.js:15054522/637

--- routes/api.js
+++ routes/api.js
@@ -13,11 +13,19 @@ var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
+  var convertHandler = new ConvertHandler();
+  
   app.route('/api/convert')
     .get(function (req, res){
       var input = req.query.input;
-      res.json(ConvertHandler.convert(input))      
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);   
       //res.json
+      if(!returnUnit && !returnNum) res.json({'invalid number and unit'})
+    else if(!returnUnit) res.json({ 'infalid unit'})
     });
     
 };
