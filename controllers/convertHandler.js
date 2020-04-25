9138235d9ea0301ae51887c2fb38d01096988956
Glitch (spangle-sky-dandelion) <none>
2020-04-25 09:24:59 +0000

    🎍🏊🏼 Checkpoint ./controllers/convertHandler.js:15054522/1284

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -7,21 +7,43 @@
 */
 
 function ConvertHandler() {
+  const numRx = /^([-]?([0-9]+[,.]?[0-9]*)([\/]([0-9]+[,.]?[0-9]*))*)/
+  const unitRx = /(gal|L|lbs|kg|km|mi)$/
   
+  const formatRet = (initNum, initUnit, returnNum, returnUnit) => {
+    return {
+      initNum: round(initNum, 5),
+      initUnit,
+      returnNum: round(returnNum, 5),
+      returnUnit
+    }
+  }
+
+  const unitConversion = {
+    'l':   m => formatRet(m, 'L',   m / GAL_L,  'gal'),
+    'gal': m => formatRet(m, 'gal', m * GAL_L,  'L'),
+    'lbs': m => formatRet(m, 'lbs', m * LBS_KG, 'kg'),
+    'kg':  m => formatRet(m, 'kg',  m / LBS_KG, 'lbs'),
+    'km':  m => formatRet(m, 'km',  m / MI_KM,  'mi'),
+    'mi':  m => formatRet(m, 'mi',  m * MI_KM,  'km')
+  }
+
+
   this.getNum = function(input) {
-    var result;
+    var result = numRx.match(input)[0];
     
     return result;
   };
   
   this.getUnit = function(input) {
-    var result;
+    var result = unitRx.match(input)[0];
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
+    var result = Object.prototype.hasOwnProperty(unitConversion, initUnit) 
+    ? unitConversion[initUnit.toLowerCase()] : 'invalid unit';
     
     return result;
   };
